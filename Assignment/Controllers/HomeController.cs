c0d079d4ace55771f65c4ca268d0060aa60512d6
Jiaqi Zhou <jzho0061@student.monash.edu>
2020-10-05 10:39:09 +1100

    Assignment

---
+++ Assignment/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+using Assignment.Models;
+
+namespace ProjectMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["idUser"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        //GET: Register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User _user)
+        {
+            if (ModelState.IsValid)
+            {
+                    return RedirectToAction("Index");
+
+
+            }
+            return View();
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
